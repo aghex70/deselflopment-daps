b4f715afbb7238068b5e7543ac9f0dca460dded9
aghex70 <aghex70@gmail.com>
2022-12-23 00:46:08 +0100

    Testing

--- database-backup.sh
+++ database-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Read the variables for the MySQL connection from the environment
-MYSQL_USERNAME=$MYSQL_USERNAME
+MYSQL_USERNAME=$MYSQL_ROOT_PASSWORD
 MYSQL_PASSWORD=$MYSQL_PASSWORD
-MYSQL_HOSTNAME=$MYSQL_HOSTNAME
+MYSQL_HOSTNAME=mysql
 MYSQL_DBNAME=$MYSQL_DBNAME
 
 # Read the destination for the backup file from the environment
@@ -13,7 +13,13 @@ BACKUP_DESTINATION="/home/ubuntu/bk/daps"
 BACKUP_FILENAME=mysql-backup-$(date +%Y-%m-%d).sql
 
 # Dump the database using mysqldump
-mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -h $MYSQL_HOSTNAME $MYSQL_DBNAME > $BACKUP_DESTINATION/$BACKUP_FILENAME
+mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -h $MYSQL_HOSTNAME $MYSQL_DBNAME > $BACKUP_FILENAME
 
-# Remove backups older than 7 days
+# Copy the backup file from the container to the host
+docker cp $(hostname):$BACKUP_FILENAME $BACKUP_DESTINATION
+
+# Remove the backup file from the container
+rm $BACKUP_FILENAME
+
+# Remove backups older than 7 days from the host
 find $BACKUP_DESTINATION -type f -mtime +7 -delete
