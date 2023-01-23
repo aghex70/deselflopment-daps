116a8eafa4b85ffa496c59b02cb318f0ddbc26df
aghex70 <aghex70@gmail.com>
2023-01-23 11:25:12 +0100

    Added onesignal-react & updated index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import {Container, Button, ModalBody, ButtonGroup, Modal} from "react-bootstrap";
 import "./App.css";
-import React from "react";
-import {goToLogin, goToRegister, skipLogin} from "./utils/helpers";
+import React, {useEffect, useState} from "react";
+import {goToLogin, goToRegister} from "./utils/helpers";
 import {LoginButtonText, RegisterButtonText, WelcomeToDapsText} from "./utils/texts";
+import OneSignal from 'react-onesignal';
 
 export default function App() {
-  skipLogin();
-  document.title = 'deselflopment - daps';
+  const [isOneSignalInitialized, setIsOneSignalInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!isOneSignalInitialized) {
+      OneSignal.init({
+        appId: "d70c53bb-aad1-461e-96a6-b6cec7b9a0d4"
+      });
+      setIsOneSignalInitialized(true);
+    }
+  }, [isOneSignalInitialized]);
 
   return (
     <Container>
@@ -16,16 +25,16 @@ export default function App() {
           <h3 style={{margin: "32px"}}>{WelcomeToDapsText}</h3>
           <ButtonGroup style={{width: "80%"}}>
             <Button
+                variant="success"
+                onClick={() => goToLogin()}
+                style={{margin: "auto", display: "block", padding: "0", textAlign: "center"}}
+            >{LoginButtonText}</Button>
+            <Button
               variant="warning"
               type="submit"
               onClick={() => goToRegister()}
               style={{margin: "auto", display: "block", padding: "0", textAlign: "center"}}
             >{RegisterButtonText}</Button>
-            <Button
-              variant="success"
-              onClick={() => goToLogin()}
-              style={{margin: "auto", display: "block", padding: "0", textAlign: "center"}}
-            >{LoginButtonText}</Button>
           </ButtonGroup>
         </ModalBody>
       </Modal>
