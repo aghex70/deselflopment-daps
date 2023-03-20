8cf42643632a274dfaf006cdb761d944218e2117
aghex70 <aghex70@gmail.com>
2023-03-20 18:09:01 +0100

    Moved scripts to directory

--- scripts/delete-completed-todos.sh
+++ scripts/delete-completed-todos.sh
@@ -8,4 +8,3 @@ MYSQL_DBNAME=$MYSQL_DATABASE
 
 # Run the DELETE query
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $MYSQL_DBNAME -e 'DELETE FROM daps_todos WHERE completed = true AND recurring = false AND end_date < DATE_SUB(CURDATE(), INTERVAL 5 DAY);'
-
