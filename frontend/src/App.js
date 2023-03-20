e5a21e89d77358777f76ced16760e0a70e07f45b
aghex70 <aghex70@gmail.com>
2023-03-20 18:04:49 +0100

    Remove onesignal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
 import {Container, Button, ModalBody, ButtonGroup, Modal} from "react-bootstrap";
 import "./App.css";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {goToLogin, goToRegister} from "./utils/helpers";
 import {LoginButtonText, RegisterButtonText, WelcomeToDapsText} from "./utils/texts";
-import OneSignal from 'react-onesignal';
 
 
 export default function App() {
-  const [isOneSignalInitialized, setIsOneSignalInitialized] = useState(false);
-
-  useEffect(() => {
-    if (!isOneSignalInitialized) {
-      OneSignal.init({
-        appId: "d70c53bb-aad1-461e-96a6-b6cec7b9a0d4"
-      });
-      setIsOneSignalInitialized(true);
-    }
-  }, [isOneSignalInitialized]);
-
   return (
     <Container>
       <Modal className='unshareModal text-center' show={true}
