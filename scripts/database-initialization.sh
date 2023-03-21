08886c06d0d19c7c81cf2ec090a3de66524fa8b6
aghex70 <aghex70@gmail.com>
2023-03-21 23:40:20 +0100

    Remove hardcoded root user & set fail_fast to true in precommit

--- scripts/database-initialization.sh
+++ scripts/database-initialization.sh
@@ -2,17 +2,18 @@
 
 # Retrieve user, password, and database name from environment variables
 user=$MYSQL_USER
+root_user=$MYSQL_ROOT_USER
 password=$MYSQL_PASSWORD
 database=$MYSQL_DATABASE
 
 # Create the database
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${database};"
+mysql -u "${MYSQL_ROOT_USER}" -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${database};"
 
 # Create the user for the database
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER ''${user}''@''%'' IDENTIFIED BY ''${password}'';"
+mysql -u "${MYSQL_ROOT_USER}" -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER ''${user}''@''%'' IDENTIFIED BY ''${password}'';"
 
 # Grant permissions to the user
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON \`${database}\`.* TO ''${user}''@''%'';"
+mysql -u "${MYSQL_ROOT_USER}" -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON \`${database}\`.* TO ''${user}''@''%'';"
 
 # Flush privileges
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+mysql -u "${MYSQL_ROOT_USER}" -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
