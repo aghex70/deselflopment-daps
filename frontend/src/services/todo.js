754fbd1790835010a5909076c2613391e0395366
aghex70 <aghex70@gmail.com>
2024-01-16 23:48:16 +0100

    Fixed services signatures

--- frontend/src/services/todo.js
+++ frontend/src/services/todo.js
@@ -20,8 +20,8 @@ const createTodo = (payload) => {
   return axios.post(TODOS_URL, payload, options);
 };
 
-const getTodo = (id, categoryId) => {
-  return axios.get(`${TODOS_URL}/${id}?category_id=${categoryId}`, options);
+const getTodo = (id) => {
+  return axios.get(`${TODOS_URL}/${id}`, options);
 };
 
 const suggestTodos = () => {
@@ -49,52 +49,42 @@ const getSuggestedTodos = () => {
   return axios.get(SUGGESTED_TODOS_URL, options);
 };
 
-const deleteTodo = (id, categoryId) => {
-  return axios.delete(`${TODOS_URL}/${id}?category_id=${categoryId}`, options);
+const deleteTodo = (id) => {
+  return axios.delete(`${TODOS_URL}/${id}`, options);
 };
 
 const updateTodo = (id, payload) => {
   return axios.put(`${TODOS_URL}/${id}`, payload, options);
 };
 
-const completeTodo = (id, categoryId) => {
-  return axios.put(
+const completeTodo = (id) => {
+  return axios.post(
     `${TODOS_URL}/${id}/complete`,
-    {
-      category_id: categoryId,
-    },
+    {},
     options
   );
 };
 
-const activateTodo = (id, categoryId) => {
-  return axios.put(
+const activateTodo = (id) => {
+  return axios.post(
     `${TODOS_URL}/${id}/activate`,
-    {
-      category_id: categoryId,
-    },
+    {},
     options
   );
 };
 
-const startTodo = (id, categoryId, payload) => {
-  return axios.put(
+const startTodo = (id) => {
+  return axios.post(
     `${TODOS_URL}/${id}/start`,
-    {
-      ...payload,
-      category_id: categoryId,
-    },
+    {},
     options
   );
 };
 
-const restartTodo = (id, categoryId, payload) => {
-  return axios.put(
+const restartTodo = (id) => {
+  return axios.post(
     `${TODOS_URL}/${id}/restart`,
-    {
-      ...payload,
-      category_id: categoryId,
-    },
+    {},
     options
   );
 };
