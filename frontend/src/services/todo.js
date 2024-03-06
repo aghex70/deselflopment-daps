8d6afa92c33f8caaeab1f487490ee0759e634621
aghex70 <aghex70@gmail.com>
2024-03-06 02:22:50 +0100

    Cleanup

--- frontend/src/services/todo.js
+++ frontend/src/services/todo.js
@@ -4,9 +4,6 @@ import {userAccessToken} from "../utils/helpers";
 const DAPS_BASE_URL = process.env.REACT_APP_API_URL;
 
 const TODOS_URL = `${DAPS_BASE_URL}api/todos`;
-const RECURRING_TODOS_URL = `${DAPS_BASE_URL}api/recurring-todos`;
-const COMPLETED_TODOS_URL = `${DAPS_BASE_URL}api/completed-todos`;
-const SUGGESTED_TODOS_URL = `${DAPS_BASE_URL}api/suggested-todos`;
 const SUGGEST_TODOS_URL = `${DAPS_BASE_URL}api/suggest`;
 
 const options = {
@@ -28,11 +25,11 @@ const suggestTodos = () => {
   return axios.post(SUGGEST_TODOS_URL, {}, options);
 };
 
-const getTodos = (id, fields) => {
+const getTodos = (fields) => {
   return axios.get(TODOS_URL, {
     ...options,
     params: {
-      category_id: id,
+      // category_id: id,
       ...(fields && fields.split('&').reduce((acc, field) => {
         const [key, value] = field.split('=');
         if (key && value) {
@@ -42,18 +39,6 @@ const getTodos = (id, fields) => {
       }, {})),
     },
   });
-};
-
-const getRecurringTodos = () => {
-  return axios.get(RECURRING_TODOS_URL, options);
-};
-
-const getCompletedTodos = () => {
-  return axios.get(COMPLETED_TODOS_URL, options);
-};
-
-const getSuggestedTodos = () => {
-  return axios.get(SUGGESTED_TODOS_URL, options);
 };
 
 const deleteTodo = (id) => {
@@ -100,9 +85,6 @@ const TodoService = {
   createTodo,
   getTodo,
   getTodos,
-  getRecurringTodos,
-  getCompletedTodos,
-  getSuggestedTodos,
   deleteTodo,
   updateTodo,
   completeTodo,
